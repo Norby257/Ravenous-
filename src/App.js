0686281e45c679284f233baa0ef555cddccdfce4
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-09-20 19:54:49 -0500

    updated API requests

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
-
 import './App.css';
+import Yelp from './Yelp';
 
-var business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
+// var business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// }
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ]
 
 class App extends Component {
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, and ${sortBy}`)
+    
   }
   render() {
     return (
